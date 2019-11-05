22447ca0cea8e0df9de5e24b40bed08d57e17ffd
Ruslan Senatorov <ruslan.senatorov@mail.ru>
2019-11-05 21:50:05 +0300

    Create addFormInputPost.php

---
+++ addFormInputPost.php
@@ -0,0 +1,5 @@
+<form action ="tests.php" method="post">
+<p>Name:<input type="text" name="name"/></p>
+<p>Age:<input type="text" name = "age"/></p>
+<p><input type="submit" name = "submite" value = "Submit"/></p>
+</form>
