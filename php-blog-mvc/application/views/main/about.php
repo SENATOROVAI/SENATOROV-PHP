17858be335db0b8b5ff28a39099d714d70c51dde
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2021-08-01 23:55:04 +0300

    commit

---
+++ php-blog-mvc/application/views/main/about.php
@@ -0,0 +1,19 @@
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Я SENATOROV</h1>
+                    <span class="subheading">РАЗРАБОТЧИК ПО</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Здесь могла бы быть ваша реклама :)</p>
+        </div>
+    </div>
+</div>
